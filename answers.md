d59ac2fd8ebef8698ae8803e5c8ee5fee98f93d3
Ollie Francis White <whiteof@dcsdk12.org>
2017-04-05 11:53:08 -0600

    A2

---
+++ answers.md
@@ -0,0 +1,4 @@
+Card and deck are separate. but deck creates an ArrayList of card objects
+3
+Create pointvalues, ranks, suits and of length 52. Ranks contains 4 1's, 4 2's and so on, up to 4 of the ace cards. Suits have 13; hearts,spades, diamonds and clubs. pointvalues contains 4 1's, 4 2's and so on, 16 10's, and 4 11's.
+Yes as the nth index of the arrays are part of one card.
--- src/Deck.java
+++ src/Deck.java
@@ -30,7 +30,13 @@ public class Deck {
 	 * @param values is an array containing all of the card point values.
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		this.cards = new ArrayList<Card>();
+        for (int i = 0; i < ranks.length; i++) {
+            Card aCard = new Card(ranks[i], suits[i], values[i]);
+            this.cards.add(aCard);
+        }
+        this.size = this.cards.size();
+        shuffle();
 	}
 
 
@@ -39,7 +45,10 @@ public class Deck {
 	 * @return true if this deck is empty, false otherwise.
 	 */
 	public boolean isEmpty() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		if (this.cards.size() == 0) {
+            return true;
+        }
+        return false;
 	}
 
 	/**
@@ -47,7 +56,7 @@ public class Deck {
 	 * @return the number of undealt cards in this deck.
 	 */
 	public int size() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return this.cards.size();
 	}
 
 	/**
@@ -64,7 +73,11 @@ public class Deck {
 	 *         previously dealt.
 	 */
 	public Card deal() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		this.size = this.size - 1;
+        if (this.size > 0) {
+            return this.cards.get(this.size);
+        }
+        return null;
 	}
 
 	/**
